0d4fe6fd8ebc9e09d648a9ad14c1b3f47a875033
Anton <antonkgeolog@gmail.com>
2022-08-09 16:57:40 +0300

    Create new Programm for checking max of 3 numbers

---
+++ Example005MaxOf3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 3 числа");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+int max = number1;
+if(max<number2){
+    max = number2;
+}
+if(max < number3){
+        max = number3;
+}
+Console.WriteLine($"Наибольшее число = {max}");
