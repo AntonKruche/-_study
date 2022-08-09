2d6d9f2e94e2dd8acba614ae7efec6f35dd3ccee
Anton <antonkgeolog@gmail.com>
2022-08-09 22:19:34 +0300

    Ex4 corrected

--- Example004MinMax/Program.cs
+++ Example004MinMax/Program.cs
@@ -1,9 +1,12 @@
-﻿Console.WriteLine("Введите 2 числа:");
+﻿/* 
+Я тут подумал, что проверку на равенство не надо, вроде в любом случае будет и минимальное и максимальное
+*/
+Console.WriteLine("Введите 2 числа:");
 int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 if(number1 >= number2){
-    Console.WriteLine($"Наибольшее число {number1}");
+    Console.WriteLine($"Наибольшее число {number1}, а наименьшее {number2}");
 }
 else{
-    Console.WriteLine($"Наибольшее число {number2}");
+    Console.WriteLine($"Наибольшее число {number2}, а наименьшее {number1}");
 }
