c30fd25c72aa34e42238e5ae98ecce96f0557fb1
Anton <antonkgeolog@gmail.com>
2022-08-09 22:36:57 +0300

    create new programm for writing list of even numbers

---
+++ Example011EvenNumbersList/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+while(count <= number -2){
+    Console.Write($"{count}, ");
+    count +=2;
+}
+Console.Write(count);
