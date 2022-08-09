172e55926f21280165302853af3250d7e768e03b
Anton <antonkgeolog@gmail.com>
2022-08-09 16:47:15 +0300

    Create new program for checking max of 2 Numbers

---
+++ Example004MinMax/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 2 числа:");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if(number1 >= number2){
+    Console.WriteLine($"Наибольшее число {number1}");
+}
+else{
+    Console.WriteLine($"Наибольшее число {number2}");
+}
