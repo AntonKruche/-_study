20697d26f8d988ce25c14c1190e18f3a4e963b0c
Anton <antonkgeolog@gmail.com>
2022-08-14 19:04:24 +0300

    create new program for calculating range between 2 dots

---
+++ Example014CalculateRange/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите координату X первой точки : ");
+int Xcoord1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y первой точки : ");
+int Ycoord1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату X второй точки : ");
+int Xcoord2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y второй точки : ");
+int Ycoord2 = int.Parse(Console.ReadLine());
+double range = Math.Sqrt(Math.Pow(Xcoord1 - Xcoord2, 2) + Math.Pow(Ycoord1 - Ycoord2, 2));
+Console.Write($"Расстояние между двумя точками равно {range}");
+
