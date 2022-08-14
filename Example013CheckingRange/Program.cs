0de7348c0853de5e2a7f91b3d786eec1eaa93fac
Anton <antonkgeolog@gmail.com>
2022-08-14 18:23:54 +0300

    add new programm for checking quarter range

---
+++ Example013CheckingRange/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите четверть : ");
+int quarter = int.Parse(Console.ReadLine());
+switch(quarter){
+    case 1:
+        Console.Write($"В четверти {quarter} возможны значения X > 0 и Y > 0");
+        break;
+    case 2:
+        Console.Write($"В четверти {quarter} возможны значения Х < 0 и Y > 0");
+        break;
+    case 3:
+        Console.Write($"В четверти {quarter} возможны значения X < 0 и Y < 0");
+        break;
+    case 4:
+        Console.Write($"В четверти {quarter} возможны значения X < 0 и Y < 0");
+        break;
+    default:
+        Console.Write("Введённое число выходит за рамки [1;4]");
+        break;    
+}
