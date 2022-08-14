5995b9412e40c1c5d4b8910c28c850781b24ef94
Anton <antonkgeolog@gmail.com>
2022-08-14 18:10:14 +0300

    Add new programm for checking quarter

---
+++ Example012XY/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите Х: ");
+int Xcoord = int.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+int Ycoord = int.Parse(Console.ReadLine());
+switch(Xcoord){
+    case > 0 :
+        if(Ycoord > 0){
+            Console.Write("1");
+        }
+        else{
+            Console.Write("4");
+        }
+        break;    
+    case < 0 :
+        if(Ycoord > 0){
+            Console.Write("2");
+        }
+        else{
+            Console.Write("3");
+        }
+        break;
+    default :
+        Console.Write("Кто-то равен нулю");
+        break;
+}
