c76bf39dae1c1767c74d6f221b255733f161a15b
Anton <antonkgeolog@gmail.com>
2022-08-14 16:27:37 +0300

    The second part of HW is complete

---
+++ HomeWork2/Example002(13)_TheLastDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Math.Abs(int.Parse(Console.ReadLine())) / 100;
+switch(number)
+{
+    case 0:
+        Console.Write("Третьей цифры нет");
+        break; 
+
+    case > 10:
+        Console.WriteLine(number % 10);
+        break;
+    default :
+        Console.WriteLine(number);
+        break;
+}     
