e41950c188fd96078e74da1f6b95d5dbb042576e
Anton <antonkgeolog@gmail.com>
2022-08-14 21:02:33 +0300

    create new programm

---
+++ Example015SquareList/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+double a;
+for(int count = 1; count < num + 1; count++){
+    a = Math.Pow(count, 2);
+    Console.Write($"{a}, ");
+}
