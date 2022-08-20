4d15fbe4596ea2dab9e8cbef5d1ea968582d407e
Anton <antonkgeolog@gmail.com>
2022-08-20 17:53:42 +0300

    New Lesson is clear

---
+++ Seminar5/Example01/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] GetArray(int size, int start, int end)
+{
+
+    int[] resultArray = new int[size];
+    Random random = new Random();
+
+    for(int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(start, end + 1);
+    }
+
+    return resultArray;
+
+}
+
+void GetSummOfArray(int[] array)
+{
+
+    int summOfPositive = 0;
+    int summOfNegative = 0;
+    foreach(int el in array)
+
+    {
+        summOfPositive += el > 0 ? el : 0;
+        summOfNegative += el < 0 ? el : 0;
+    }
+    Console.Write($"Сумма положительных чисел в массиве равна: {summOfPositive}, а сумма отрицательных значений равна: {summOfNegative}");
+
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+Console.Write("Введите нижнюю границу для генератора случайных чисел: ");
+int start = int.Parse(Console.ReadLine());
+Console.Write("Введите верхнюю границу для генератора случайных чисел: ");
+int end = int.Parse(Console.ReadLine());
+
+int[] array = GetArray(size, start, end);
+GetSummOfArray(array);
---
+++ Seminar5/Example05/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] GetArray(int size, int start, int end)
+{
+
+    int[] resultArray = new int[size];
+    Random random = new Random();
+
+    for(int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(start, end + 1);
+    }
+
+    return resultArray;
+
+}
+
+int[] GetMultiArray(int[] array)
+{
+    int size = array.Length / 2;
+    if(array.Length % 2 == 1)
+    {
+        size += 1;
+    }
+    int[] resultArray = new int[size];
+    int multi = 0;
+    int start = 0;
+    int end = array.Length -1;
+    while(start < end)
+    {
+        end = (array.Length - 1) - start;
+        multi = array[start] * array[end];
+        resultArray[start] = multi;
+        start +=1;
+    }
+
+    return resultArray;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+Console.Write("Введите нижнюю границу для генератора случайных чисел: ");
+int start = int.Parse(Console.ReadLine());
+Console.Write("Введите верхнюю границу для генератора случайных чисел: ");
+int end = int.Parse(Console.ReadLine());
+
+int[] array = GetArray(size, start, end);
+Console.WriteLine(String.Join(" ", array));
+array = GetMultiArray(array);
+Console.Write("[{0}]", String.Join(", ", array));
