3c413754829bda4ee14271cd778eb4e9fbe92154
Anton <antonkgeolog@gmail.com>
2022-08-24 19:23:14 +0300

    Seminar6 is complete

---
+++ Seminar6/Example02/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+bool CheckTriangle(int num1,int num2,int num3)
+{
+    return num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1;
+}
+
+if(CheckTriangle(a ,b , c))
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
---
+++ Seminar6/Example04/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] GetFiboNum(int count)
+{
+    int[] result = new int[count];
+    result[0] = 0;
+    result[1] = 1;
+    for(int i = 2; i < result.Length; i++)
+    {   
+        result[i] = result[i-1] + result[i-2];
+        //Console.WriteLine(result[i]);
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int countOf = int.Parse(Console.ReadLine());
+
+int[] array = GetFiboNum(countOf);
+Console.WriteLine("{0}", String.Join(" ", array));
+//Console.Write(String.Join(" ", array));
